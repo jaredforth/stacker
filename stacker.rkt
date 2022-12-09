f21a10e6c60c6c3e037aa5482da57da828a8a61e
Jared Forth <jared.forth@output.com>
2022-12-09 12:26:59 -0600

    define handle function

--- stacker.rkt
+++ stacker.rkt
@@ -22,3 +22,11 @@
 
 (define (push-stack! arg)
   (set! stack (cons arg stack)))
+
+(define (handle [arg #f])
+  (cond
+    [(number? arg) (push-stack! arg)]
+    [(or (equal? + arg) (equal? * arg))
+     (define op-result (arg (pop-stack!) (pop-stack!)))
+     (push-stack! op-result)]))
+(provide handle)
