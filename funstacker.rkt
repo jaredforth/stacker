8a301f37a24397c093461784d8bdcc030b30089f
Jared Forth <jared.forth@output.com>
2022-12-12 15:51:14 -0600

    functional style rewrite

--- funstacker.rkt
+++ funstacker.rkt
@@ -13,22 +13,16 @@
      (display (first HANDLE-ARGS-EXPR))))
 (provide (rename-out [funstacker-module-begin #%module-begin]))
 
-(define stack empty)
-
-(define (pop-stack!)
-  (define arg (first stack))
-  (set! stack (rest stack))
-  arg)
-
-(define (push-stack! arg)
-  (set! stack (cons arg stack)))
-
-(define (handle [arg #f])
-  (cond
-    [(number? arg) (push-stack! arg)]
-    [(or (equal? * arg) (equal? + arg))
-     (define op-result (arg (pop-stack!) (pop-stack!))) 
-     (push-stack! op-result)]))
-(provide handle)
+(define (handle-args . args)
+  (for/fold ([stack-acc empty])
+            ([arg (in-list args)]
+             #:unless (void? arg))
+    (cond
+      [(number? arg) (cons arg stack-acc)]
+      [(or (equal? * arg) (equal? + arg))
+       (define op-result
+         (arg (first stack-acc) (second stack-acc)))
+       (cons op-result (drop stack-acc 2))])))
+(provide handle-args)
 
 (provide + *)
