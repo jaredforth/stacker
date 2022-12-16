f0589a76af9d0e37e84e1e23d0ec65c34faf5027
Jared Forth <jared.forth@output.com>
2022-12-16 13:58:55 -0600

    define stackerizer

--- stackerizer.rkt
+++ stackerizer.rkt
@@ -3,5 +3,14 @@
 
 (define-macro (stackerizer-mb EXPR)
   #'(#%module-begin
-     EXPR))
+     (for-each displayln (reverse (flatten EXPR)))))
 (provide (rename-out [stackerizer-mb #%module-begin]))
+
+(define-macro (define-op OP)
+  #'(define-macro-cases OP
+      [(OP FIRST) #'FIRST]
+      [(OP FIRST NEXT (... ...))
+       #'(list 'OP FIRST (OP NEXT (... ...)))]))
+
+(define-op +)
+(define-op *)
